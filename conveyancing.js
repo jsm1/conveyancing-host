92769fd365ab56b1811dac3fbb64e299cdf74dcc
Jeremy Morris <jeremy@Jeremys-MacBook-Pro.local>
2018-07-21 15:42:51 +0200

    Adding loqate js

--- conveyancing.js
+++ conveyancing.js
@@ -1,1 +1,5 @@
-var data = {};
+ var fields = [{ element: "postcode", field: "Postcode", mode: "pca.fieldMode.POPULATE" }];
+
+ var settings = { key: "WN86-WF99-YA99-TT77", search: { countries: "AUS" }, setCountryByIP: false };
+
+ var control = new pca.Address(field, settings);
