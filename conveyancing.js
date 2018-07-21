420483a5e047c592848568568f20116d8077c9f7
Jeremy Morris <jeremy@Jeremys-MacBook-Pro.local>
2018-07-21 15:44:51 +0200

    Adding loqate js

--- conveyancing.js
+++ conveyancing.js
@@ -1,5 +1,11 @@
- var fields = [{ element: "postcode", field: "Postcode", mode: "pca.fieldMode.POPULATE" }];
+ 
+$(document).ready(function() {
+	var loqateControl = configureLoqate();
+});
 
- var settings = { key: "WN86-WF99-YA99-TT77", search: { countries: "AUS" }, setCountryByIP: false };
-
- var control = new pca.Address(field, settings);
+function configureLoqate() {
+	var fields = [{ element: "postcode", field: "Postcode", mode: "pca.fieldMode.POPULATE" }];
+	var settings = { key: "WN86-WF99-YA99-TT77", search: { countries: "AUS" }, setCountryByIP: false };
+	var control = new pca.Address(fields, settings);
+	return control;
+}
